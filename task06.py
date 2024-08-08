b1dc2964e5966f9d46166dfdde051cd80efea129
Serj Pyshkin <IndianaCornell@gmail.com>
2024-08-08 16:11:01 +0300

    Update task06.py

--- task06.py
+++ task06.py
@@ -1,91 +1,59 @@
-import matplotlib.pyplot as plt
-import networkx as nx
-import matplotlib.patches as patches
-
-class TreeNode:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-def dfs_visualization(root):
-    color_map = {}
-    node_colors = {}
-    stack = [(root, 0)]  
-    level = 0
-
-    while stack:
-        node, level = stack.pop()
-        if node:
-            color = "#{:02x}{:02x}{:02x}".format(0, 255 - level * 50, level * 30)
-            color_map[node.value] = color
-            node_colors[node.value] = color
-            
-            # Додаємо дітей до стека
-            if node.right:
-                stack.append((node.right, level + 1))
-            if node.left:
-                stack.append((node.left, level + 1))
-
-    return color_map
+items = {
+    "pizza": {"cost": 50, "calories": 300},
+    "hamburger": {"cost": 40, "calories": 250},
+    "hot-dog": {"cost": 30, "calories": 200},
+    "pepsi": {"cost": 10, "calories": 100},
+    "cola": {"cost": 15, "calories": 220},
+    "potato": {"cost": 25, "calories": 350},
+}
 
 
-def bfs_visualization(root):
-    color_map = {}
-    node_colors = {}
-    queue = [(root, 0)] 
-    level = 0
+def greedy_algorithm(items, budget):
+    total_cost = 0
+    total_calories = 0
+    selected_items = []
 
-    while queue:
-        node, level = queue.pop(0)
-        if node:
-            color = "#{:02x}{:02x}{:02x}".format(0, 255 - level * 50, level * 30)
-            color_map[node.value] = color
-            node_colors[node.value] = color
-            
-            if node.left:
-                queue.append((node.left, level + 1))
-            if node.right:
-                queue.append((node.right, level + 1))
-
-    return color_map
+    sorted_items = sorted(items.items(), key=lambda x: x[1]['calories'] / x[1]['cost'], reverse=True)
 
 
-def visualize_tree(root, colors, title):
-    G = nx.DiGraph()
-    pos = {}
 
-    def add_edges(node, pos, x=0, y=0, layer=1):
-        if node:
-            G.add_node(node.value)
-            pos[node.value] = (x, y)
-            if node.left:
-                G.add_edge(node.value, node.left.value)
-                add_edges(node.left, pos, x - 1 / layer, y - 1, layer + 1)
-            if node.right:
-                G.add_edge(node.value, node.right.value)
-                add_edges(node.right, pos, x + 1 / layer, y - 1, layer + 1)
-        return pos
+    for item, data in sorted_items:
+        if total_cost + data['cost'] <= budget:
+            selected_items.append(item)
+            total_calories += data['calories']
+            total_cost += data['cost']
+    
+    return selected_items, total_calories, total_cost
 
-    add_edges(root, pos)
 
-    plt.figure(figsize=(10, 6))
-    nx.draw(G, pos, with_labels=True, node_size=2000, node_color=[colors.get(node, '#ffffff') for node in G.nodes], font_size=16, font_weight='bold', edge_color='gray')
-    plt.title(title)
-    plt.show()
+budget = 100
+selected_items, total_calories, total_cost = greedy_algorithm(items, budget)
+print("Вибрані страви:", selected_items)
+print("Загальна калорійність:", total_calories)
+print("Загальна вартість:", total_cost)
 
-# Тестове дерево
-root = TreeNode(1)
-root.left = TreeNode(2)
-root.right = TreeNode(3)
-root.left.left = TreeNode(4)
-root.left.right = TreeNode(5)
-root.right.right = TreeNode(6)
 
-# обхід у глибину
-dfs_colors = dfs_visualization(root)
-visualize_tree(root, dfs_colors, "DFS Visualization")
+def dynamic_programming(items, budget):
+    dp = [0] * (budget + 1)
+    selected_items = [[] for _ in range(budget + 1)]
 
-#обхід в ширину
-bfs_colors = bfs_visualization(root)
-visualize_tree(root, bfs_colors, "BFS Visualization")
+    for item, info in items.items():
+        cost = info["cost"]
+        calories = info["calories"]
+        for current_budget in range(budget, cost - 1, -1):
+            if dp[current_budget - cost] + calories > dp[current_budget]:
+                dp[current_budget] = dp[current_budget - cost] + calories
+                selected_items[current_budget] = selected_items[
+                    current_budget - cost
+                ] + [item]
+
+    optimal_items = selected_items[budget]
+    total_calories = dp[budget]
+
+    return optimal_items, total_calories
+
+
+selected_items, total_calories = dynamic_programming(items, budget)
+print("\nДинамічне програмування")
+print("Вибрані страви:", selected_items)
+print("Загальна калорійність:", total_calories,"\n")
